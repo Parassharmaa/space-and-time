6b4c89903a7e92fcc9d7b4aea95611a5e1408eb5
paraz <mail2paras.s@gmail.com>
2018-08-29 01:03:16 +0530

    [CHORE] files added

--- frontend/src/store/modules/app/actions.js
+++ frontend/src/store/modules/app/actions.js
@@ -1,23 +1,3 @@
 import * as types from './constants'
 
-export const set_loading = (yes = true) => {
-  return {
-    type: types.SET_LOADING,
-    payload: yes
-  }
-}
-
-export const set_application_modal = (payload) => {
-  return {
-    type: types.SET_APPLICATION_MODAL,
-    payload: payload
-  }
-}
-
-export const set_complaint_modal = (payload) => {
-  return {
-    type: types.SET_COMPLAINT_MODAL,
-    payload: payload
-  }
-}
 
--- frontend/src/store/modules/app/reducer.js
+++ frontend/src/store/modules/app/reducer.js
@@ -1,33 +1,5 @@
-import { handleActions } from 'redux-actions'
-import { SET_LOADING, SET_APPLICATION_MODAL, SET_COMPLAINT_MODAL } from './constants'
+import { handleActions } from "redux-actions";
 
+const initialState = {};
 
-const initialState = {
-  loading: false,
-  applicationModal: false,
-  complaintModal: false
-}
-
-export default handleActions(
-  {
-    [SET_LOADING]: (state = initialState, action) => {
-      return {
-        ...state,
-        loading: action.payload
-      }
-    },
-    [SET_APPLICATION_MODAL]: (state = initialState, action) => {
-      return {
-        ...state,
-        applicationModal: action.payload
-      }
-    },
-    [SET_COMPLAINT_MODAL]: (state = initialState, action) => {
-      return {
-        ...state,
-        complaintModal: action.payload
-      }
-    }
-  },
-  initialState
-)
+export default handleActions({}, initialState);
