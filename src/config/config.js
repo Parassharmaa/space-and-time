ad981beb4e7c08c511c6b467a780d7711a089b30
paraz <mail2paras.s@gmail.com>
2018-08-31 19:39:23 +0530

    [UPDATE] google Oauth Key

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,7 @@ const config = {
   mapboxAPI:
     "pk.eyJ1IjoicGFyYXoiLCJhIjoiY2psOXQ1Z3Y3MHF1ZTNycGRhdXo4amtjbCJ9.0OK1sq_I9rnJhibgwMu7xw",
   googleClientId:
-    "135024769582-a5pba6g3jnpmn91enqoocssjnog1ocmj.apps.googleusercontent.com",
+    "135024769582-jilp38tvekqnt0l92p955o3h2856u2re.apps.googleusercontent.com",
   baseUrl: baseUrl
 };
 
