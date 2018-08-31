029c3ed64b229e8ac1dbc2898f4d5c1e85a20b23
paraz <mail2paras.s@gmail.com>
2018-08-31 14:34:56 +0530

    [ADD] heroku init

--- config/__init__.py
+++ config/__init__.py
@@ -1,16 +1,11 @@
 import sys
 import os
-from .env import env
+
 
 def load_config():
     current_dir = os.path.abspath('./')
 
-    os.environ.update({
-        "ROOT_PATH": current_dir
-    })
-
-    os.environ['secret_key'] = env['secret_key']
-    os.environ['mongo_db'] = env['mongo_db']
+    os.environ.update({"ROOT_PATH": current_dir})
 
     sys.path.append(os.path.join(os.environ.get('ROOT_PATH'), 'modules'))
     sys.path.append(os.path.join(os.environ.get('ROOT_PATH'), 'resources'))
